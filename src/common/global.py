a35751b244d3fcbc34eff457f7033e325ef68500
joyghosh <joy.ghosh@tiatros.com>
2015-09-26 20:41:25 +0530

    presence: add and remove peers

--- src/common/global.py
+++ src/common/global.py
@@ -8,7 +8,8 @@ class Global(object):
     '''
     Shared data-structure for book keeping.
     '''
-    channels = {}
+    channels={}
+    peers={}
     
     def __init__(self, params):
         '''
@@ -16,7 +17,7 @@ class Global(object):
         '''
     
     '''Returns the number of channels in-memory.'''
-    def get_num_of_channels(self):
+    def num_of_channels(self):
         return len(Global.channels)
     
     '''check if the channel name already exists.'''
@@ -34,10 +35,25 @@ class Global(object):
             return None
     
     '''Add a new channel to global data structure.'''
-    def put_channel(self,channel,channel_object):
+    def put_channel(self,channel,channelObject):
         if(Global.channel_exists(self, channel)):
             return False
         else:
-            Global.channels[channel] = channel_object
+            Global.channels[channel] = channelObject
             return True
-            
+    
+    '''Put new peer.'''         #Food for thought. Use Redis or MongoDb.
+    def put_peer(self, peerId, peerWSObject):
+        if(peerId not in Global.peers):
+            Global.peers[peerId] = peerWSObject
+            return True
+        else:
+            return False
+        
+    '''Remove peer.'''
+    def remove_peer(self, peerId):
+        if(peerId in Global.peers):
+            Global.peers.pop(peerId)
+            return True
+        else:
+            return False
