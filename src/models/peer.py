0c5c5e855899ae60034639c13dfd3b124fc14c66
joyghosh <joy.ghosh@tiatros.com>
2015-10-11 23:03:58 +0530

    presence channel

--- src/models/peer.py
+++ src/models/peer.py
@@ -4,14 +4,15 @@ Created on 02-Oct-2015
 @author: joyghosh
 '''
 
+
 class Peer(object):
     '''
-    Peer model representing the node object.
+    Peer model representing the end-point object.
     '''
-
-
-    def __init__(self, params):
+    
+    def __init__(self, user_id, socket):
         '''
         Constructor
         '''
-        
+        self.user_id = user_id
+        self.socket = socket
